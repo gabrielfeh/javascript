58352156785b29a499d49f4961c553906d6f2309
Gabriel Felipe da Silva <gabrierebe@gmail.com>
2023-01-09 11:34:26 -0300

    funções

---
+++ aula16/funcao01.js
@@ -0,0 +1,11 @@
+function parimpar(n){
+    if (n%2 == 0){
+        return 'Par!'
+    }else{
+        return 'Impar!'
+    }
+}
+ let res = parimpar(4)
+ console.log(res)
+ //Ou pode ser feito como abaixo
+ console.log(parimpar(223))
